22b2a4ba7ed088aab9ed0635c77aa5244354e21b
theosarbachfischer <theosarbach0407@gmail.com>
2026-01-26 11:57:48 +0100

    test

---
+++ src/main/java/org/cao/backend/FileHelper.java
@@ -0,0 +1,58 @@
+package org.cao.backend;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class FileHelper {
+
+    // =============== Attributs ===============
+
+    private File file;
+
+    // =============== Constructeur ===============
+
+    public FileHelper(File file) {
+        this.file = file;
+    }
+
+    // =============== Méthodes ===============
+
+    public List<String> readAllLines() {
+        List<String> lines = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return lines;
+    }
+
+    public List<String> readAllCodeCAN() {
+        List<String> codeCAN = new ArrayList<>();
+
+        for (String line : readAllLines()) {
+            String code = line.split(";")[0];
+            codeCAN.add(code);
+        }
+
+        return codeCAN;
+    }
+
+    // =============== Accesseurs ===============
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+}
--- src/main/java/org/cao/backend/db/DatabaseManager.java
+++ src/main/java/org/cao/backend/db/DatabaseManager.java
@@ -1,20 +1,28 @@
 package org.cao.backend.db;
 
 import org.cao.backend.BackendLogic;
+import org.cao.backend.FileHelper;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.*;
 
 public class DatabaseManager {
 
-    private static final String URL = BackendLogic.readProperty("db.url");
-    private static final String USER = BackendLogic.readProperty("db.user");
-    private static final String PASSWORD = BackendLogic.readProperty("db.password");
+    private static final String URL = readProperty("db.url");
+    private static final String USER = readProperty("db.user");
+    private static final String PASSWORD = readProperty("db.password");
+
+    public static final String ARTICLES_OUT_BONG_PATH = readProperty("file.out.path");
+    public static final String ARTICLES_IN_BONG_PATH = readProperty("file.in.path");
+    public static final String DIRECTORY_PATH = readProperty("directory.path");
+
+    public static final List<String> AUTHORIZED_FOLDERS_NAMES = separateAllAuthorizedFolders(readProperty("authorized.explore.folders"));
 
     static void main() {
-
+        updateDatabase();
     }
 
     private static void updateDatabase() {
@@ -22,7 +30,8 @@ public class DatabaseManager {
 
         try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD); Statement statement = con.createStatement()) {
 
-
+            //fillFichierTable(con, statement);
+            fillArticleCANTable(con, statement);
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -36,5 +45,204 @@ public class DatabaseManager {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void fillArticleCANTable(Connection con, Statement statement) throws SQLException {
+        File fileOut = new File(ARTICLES_OUT_BONG_PATH);
+        FileHelper fileHelper = new FileHelper(fileOut);
+
+        List<String> lines = fileHelper.readAllLines();
+        int i = 0;
+        int maxLines = lines.size();
+
+        for (String line : lines) {
+            String codeCAN = line.split(";")[0];
+            String descCAN = line.split(";")[1];
+            String query;
+
+            if (descCAN.isEmpty() || descCAN.equals(" ")) continue;
+
+            boolean alreadyInserted = codeCANAlreadyExist(con, codeCAN);
+
+            if (alreadyInserted) {
+                query = "UPDATE ArticleCAN SET DescCAN = ? WHERE CodeCAN = ?";
+            } else {
+                query = "INSERT INTO ArticleCAN(CodeCAN, LP, AchFab, Statut, DescCAN, DescCANUK, UM, Matiere) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+            }
+
+            try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+                if (alreadyInserted) {
+                    preparedStatement.setString(1, descCAN);
+                    preparedStatement.setString(2, codeCAN);
+                } else {
+                    preparedStatement.setString(1, codeCAN);
+                    preparedStatement.setString(2, ".");
+                    preparedStatement.setString(3, ".");
+                    preparedStatement.setString(4, ".");
+                    preparedStatement.setString(5, descCAN);
+                    preparedStatement.setString(6, ".");
+                    preparedStatement.setString(7, ".");
+                    preparedStatement.setString(8, ".");
+                }
+
+                preparedStatement.execute();
+            }
+
+            i++;
+            int percentage = (int) (((float) i / maxLines) * 100);
+            System.out.print("\rProgression: " + percentage + "%");
+        }
+    }
+
+    private static void fillFichierTable(Connection con, Statement statement) throws SQLException {
+        File mainDirectory = new File(BackendLogic.DIRECTORY_PATH);
+        List<File> files = returnFilesInDirectory(mainDirectory);
+        int i = 0;
+        int maxFiles = files.size();
+
+        for (File file : files) {
+            String fileName = file.getName();
+            String fileType = convertParentToType(file.getParentFile().getName());
+            String codeCAN = getCodeCAN(fileName);
+            String revision = getRevision(fileName);
+            String filePath = file.getAbsolutePath();
+
+            String checkQuery = "SELECT COUNT(*) FROM Fichier WHERE CodeCAN = ? AND Revision = ?";
+            boolean exists = false;
+
+            try (PreparedStatement checkStmt = con.prepareStatement(checkQuery)) {
+                checkStmt.setString(1, codeCAN);
+                checkStmt.setString(2, revision);
+                ResultSet rs = checkStmt.executeQuery();
+                if (rs.next() && rs.getInt(1) > 0) {
+                    exists = true;
+                }
+            }
+
+            String query;
+            if (exists) {
+                query = "UPDATE Fichier SET Nom = ?, RepertoireNom = ?, Type = ?, Chemin = ?, Taille = ?, Page = ?, Lien = ?, DernRev = ?, IncoRev = ?, CodeCAN_Parent = ?, Revision_Parent = ? WHERE CodeCAN = ? AND Revision = ?";
+            } else {
+                query = "INSERT INTO Fichier(Nom, RepertoireNom, Type, Chemin, Taille, CodeCAN, Revision, Page, Lien, DernRev, IncoRev, CodeCAN_Parent, Revision_Parent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            }
+
+            try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+                preparedStatement.setString(1, fileName);
+                preparedStatement.setString(2, ".");
+                preparedStatement.setString(3, fileType);
+                preparedStatement.setString(4, ".");
+                preparedStatement.setInt(5, 0);
+
+                if (exists) {
+                    preparedStatement.setInt(6, 0);
+                    preparedStatement.setString(7, filePath);
+                    preparedStatement.setString(8, "0");
+                    preparedStatement.setString(9, "0");
+                    preparedStatement.setString(10, ".");
+                    preparedStatement.setString(11, ".");
+                    preparedStatement.setString(12, codeCAN);
+                    preparedStatement.setString(13, revision);
+                } else {
+                    preparedStatement.setString(6, codeCAN);
+                    preparedStatement.setString(7, revision);
+                    preparedStatement.setInt(8, 0);
+                    preparedStatement.setString(9, filePath);
+                    preparedStatement.setString(10, "0");
+                    preparedStatement.setString(11, "0");
+                    preparedStatement.setString(12, ".");
+                    preparedStatement.setString(13, ".");
+                }
+
+                preparedStatement.execute();
+            }
+
+            i++;
+            int percentage = (int) (((float) i / maxFiles) * 100);
+            System.out.print("\rProgression: " + percentage + "%");
+        }
+    }
+
+    private static boolean codeCANAlreadyExist(Connection con, String codeCAN) throws SQLException {
+        String sql = "SELECT 1 FROM ArticleCAN WHERE CodeCAN = ?";
+
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, codeCAN);
+            try (ResultSet rs = ps.executeQuery()) {
+                return rs.next();
+            }
+        }
+    }
+
+
+    public static List<File> returnFilesInDirectory(File directory) {
+        return returnFilesInDirectory(directory, new HashSet<>());
+    }
+
+    private static List<File> returnFilesInDirectory(File directory, Set<String> visited) {
+        if (!visited.add(directory.getAbsolutePath())) {
+            return new ArrayList<>();
+        }
+
+        File[] files = directory.listFiles();
+        List<File> allSubFiles = new ArrayList<>();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    if (isFolderAuthorized(file)) {
+                        allSubFiles.addAll(returnFilesInDirectory(file, visited));
+                    }
+                } else {
+                    allSubFiles.add(file);
+                }
+            }
+        }
+
+        return allSubFiles;
+    }
+
+    private static String getCodeCAN(String fileName) {
+        return fileName.split("_")[0];
+    }
+
+    private static String getRevision(String fileName) {
+        int lastDot = fileName.lastIndexOf('.');
+        String extension = fileName.substring(lastDot);
+
+        return fileName.split("_")[fileName.split("_").length - 1].split(extension)[0];
+    }
+
+    private static boolean isFolderAuthorized(File file) {
+        List<String> foldersNameAuthorized = BackendLogic.AUTHORIZED_FOLDERS_NAMES;
+        String folderName = file.getName();
+        return foldersNameAuthorized.contains(folderName);
+    }
+
+    public static String convertParentToType(String parentName) {
+        return switch(parentName) {
+            case "ELEC_online" -> "ELEC";
+            case "CONFIG_online" -> "CONFIG";
+            case "PDFS_online" -> "PLAN";
+            case "ECLATE_online" -> "PLAN_ECL";
+            case "SCANNE_online" -> "SCAN";
+            default -> "Non-renseigné";
+        };
+    }
+
+    public static String readProperty(String property) {
+        Properties props = new Properties();
+
+        try (InputStream fis = BackendLogic.class.getClassLoader().getResourceAsStream("config.properties")) {
+            props.load(fis);
+            return props.getProperty(property);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Erreur : Propriété introuvable.";
+    }
+
+    private static List<String> separateAllAuthorizedFolders(String filesName) {
+        return List.of(filesName.split(";"));
     }
 }
